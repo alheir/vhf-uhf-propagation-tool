b1ab7ff888284c711776e5716df23cb09cebc323
mariano <mdolhare@itba.edu.ar>
2024-11-22 20:32:17 -0300

    Cursores funcionando

--- gui.py
+++ gui.py
@@ -2,9 +2,44 @@ from PyQt6.QtWidgets import QApplication, QMainWindow, QTableWidget, QTableWidge
 from design import Ui_MainWindow  
 from matplotlib import pyplot as plt
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backend_bases import MouseEvent
 from PyQt6.QtCore import pyqtSlot
 from calculations import PropagationCalculator
 import numpy as np
+import mplcursors
+
+class Cursor:
+    """
+    A cross hair cursor.
+    """
+    def __init__(self, ax):
+        self.ax = ax
+        self.horizontal_line = ax.axhline(color='w', lw=0.8, ls='--')
+        self.vertical_line = ax.axvline(color='w', lw=0.8, ls='--')
+        # text location in axes coordinates
+        self.text = ax.text(0.72, 0.9, '', transform=ax.transAxes)
+
+    def set_cross_hair_visible(self, visible):
+        need_redraw = self.horizontal_line.get_visible() != visible
+        self.horizontal_line.set_visible(visible)
+        self.vertical_line.set_visible(visible)
+        self.text.set_visible(visible)
+        return need_redraw
+
+    def on_mouse_move(self, event):
+        if not event.inaxes:
+            need_redraw = self.set_cross_hair_visible(False)
+            if need_redraw:
+                self.ax.figure.canvas.draw()
+        else:
+            self.set_cross_hair_visible(True)
+            x, y = event.xdata, event.ydata
+            # update the line positions
+            self.horizontal_line.set_ydata([y])
+            self.vertical_line.set_xdata([x])
+            self.text.set_text(f'x={x:1.2f}, y={y:1.2f}')
+            self.ax.figure.canvas.draw()
+
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
@@ -62,14 +97,14 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             }
             
             antenna_type = antenna_type_map[self.ui.antenna_type_input.currentText()]
-            polarization = polarization_map[self.ui.polarization_input.currentText()]
+            polarization = self.ui.polarization_input.text()
             
             conductivity = float(self.ui.conductivity_input.text())
             permitivity = float(self.ui.permittivity_input.text())
             roughness = float(self.ui.terrain_roughness_input.text())
             
             antenna_type = self.ui.antenna_type_input.currentText()
-            polarization = self.ui.polarization_input.currentText()
+            polarization = self.ui.polarization_input.text()
             earth_radius_factor = float(self.ui.earth_radius_factor_input.text())
 
             calculator = PropagationCalculator(freq, 
@@ -108,18 +143,25 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             self.figure1.clear()
             ax1 = self.figure1.add_subplot(111)
             ax1.plot(distances / 1000, P_rs)  # Convertir de metros a km
+            mplcursors.cursor() 
             ax1.set_title('Potencia recibida vs Distancia')
             ax1.set_xlabel('Distancia (km)')
             ax1.set_ylabel(f'Potencia recibida ({self.ui.power_unit_input.currentText()})')
             if self.ui.scatter_checkbox.isChecked():
                 ax1.scatter(distances / 1000, P_rs, color='red', marker='x', alpha=0.5)
             ax1.grid()
+            
+            
+            cursor1 = Cursor(ax1)
+            self.canvas1.mpl_connect('motion_notify_event', cursor1.on_mouse_move)
+            
             self.canvas1.draw()
 
             # gráfico de campo eléctrico vs distancia
             self.figure2.clear()
             ax2 = self.figure2.add_subplot(111)
             ax2.plot(distances / 1000, E_totals)  # Convertir de metros a km
+            mplcursors.cursor() 
             ax2.set_title('Campo eléctrico vs Distancia')
             ax2.set_xlabel('Distancia (km)')
             ax2.set_ylabel('Campo eléctrico (V/m)')
@@ -127,6 +169,10 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             if self.ui.scatter_checkbox.isChecked():
                 ax2.scatter(distances / 1000, E_totals, color='red', marker='x', alpha=0.5)
             ax2.grid()
+            
+            cursor2 = Cursor(ax2)
+            self.canvas2.mpl_connect('motion_notify_event', cursor2.on_mouse_move)
+            
             self.canvas2.draw()
             
             # tabla de variación con la distancia
@@ -163,6 +209,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             self.figure3.clear()
             ax3 = self.figure3.add_subplot(111)
             ax3.plot(heights, P_rs_height)
+            mplcursors.cursor() 
             ax3.axvline(x=fixed_height, color='r', linestyle='--', label=f'{fixed_label}')
             ax3.set_title(f'Potencia recibida vs Altura de la antena {self.ui.height_vary_input.currentText()}')
             ax3.set_xlabel(f'Altura de la antena {self.ui.height_vary_input.currentText()} (m)')
@@ -171,12 +218,17 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                 ax3.scatter(heights, P_rs_height, color='red', marker='x', alpha=0.5)
             ax3.legend()
             ax3.grid()
+            
+            cursor3 = Cursor(ax3)
+            self.canvas3.mpl_connect('motion_notify_event', cursor3.on_mouse_move)
+            
             self.canvas3.draw()
 
             # gráfico de campo eléctrico vs altura de la antena
             self.figure4.clear()
             ax4 = self.figure4.add_subplot(111)
             ax4.plot(heights, E_totals_height)
+            mplcursors.cursor() 
             ax4.axvline(x=fixed_height, color='r', linestyle='--', label=f'{fixed_label}')
             ax4.set_title(f'Campo eléctrico vs Altura de la antena {self.ui.height_vary_input.currentText()}')
             ax4.set_xlabel(f'Altura de la antena {self.ui.height_vary_input.currentText()} (m)')
@@ -186,6 +238,10 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                 ax4.scatter(heights, E_totals_height, color='red', marker='x')
             ax4.legend()
             ax4.grid()
+            
+            cursor4 = Cursor(ax4)
+            self.canvas4.mpl_connect('motion_notify_event', cursor4.on_mouse_move)
+            
             self.canvas4.draw()
             
             # tabla de variación con la altura
