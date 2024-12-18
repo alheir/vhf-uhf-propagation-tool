6fda76d73e87175c958c78f7cba31bad9cb99eb5
Ale Heir <aheir@itba.edu.ar>
2024-12-18 02:42:37 -0300

    feat: hardcoded min Psi
    Co-authored-by: FAgrippino <FAgrippino@users.noreply.github.com>

--- src/calculations.py
+++ src/calculations.py
@@ -73,7 +73,9 @@ class PropagationCalculator:
         hr = height_rx
 
         if r >= self.LOS_point_to_point:
-            return None, None, None, None, None, None
+            return None, None, None, None, None, None, None
+        
+        # if r < self.LOS_point_to_point:
         
         p = (2 / np.sqrt(3)) * np.sqrt(re * (hr + ht) + r*r/4)
         
@@ -85,82 +87,172 @@ class PropagationCalculator:
         phi1 = r1/re
         phi2 = r2/re
         
+        self.ht_eff = ht - 0.5 * re * phi1**2
+        self.hr_eff = hr - 0.5 * re * phi2**2
+        
         R1 = np.sqrt(ht**2 + 4 * re * (re + ht) * (np.sin(phi1 / 2)**2))
         R2 = np.sqrt(hr**2 + 4 * re * (re + hr) * (np.sin(phi2 / 2)**2))
         
         Rd = np.sqrt((hr - ht)**2 + 4 * (re + hr) * (re + ht) * (np.sin((phi1 + phi2) / 2)**2)) # distancia entre Tx y Rx, real
-         
+        
         
         Delta_R = R1 + R2 - Rd
         
         sqrt_arg = Delta_R * (R1 + R2 + Rd) / (4 * R1 * R2)
         arcsin_arg = np .sqrt(sqrt_arg) 
         Psi = np.arcsin(arcsin_arg)
- 
-        Delta = self.Beta * Delta_R                                     # diferencia de camino óptico
+        # Psi = np.arcsin(ht_eff / R1)
         
+        lim_psi = np.deg2rad(0.1)
+        Psi = Psi if Psi > lim_psi else lim_psi
 
-        P_t = self.tx_power * self.antenna_tx_gain  # potencia de transmisión
-        E_zero = np.sqrt(ETA_ZERO * P_t / (4 * np.pi)) / Rd
+        Delta = self.Beta * Delta_R                                     # diferencia de camino óptico
+    
 
         epsilon_c = self.epsilon_r - 1j * self.sigma / (self.w * EPSILON_ZERO)
-
-
-        # Tierra esférica
-        D_factor = 1 / np.sqrt(1 + (2 * r1 * r2)/(re * r * np.sin(Psi)))
         
-        # Rugosidad
-        roughness_factor = np.exp(-2 * (self.Beta * self.roughness * np.sin(Psi))**2)
-
         # Coeficientes de reflexión
         if self.antenna_pol == ANTENNA_POL_H:
             Gamma = (epsilon_c * np.sin(Psi) - np.sqrt(epsilon_c - np.cos(Psi)**2)) / (epsilon_c * np.sin(Psi) + np.sqrt(epsilon_c - np.cos(Psi)**2))
         elif self.antenna_pol == ANTENNA_POL_V:
             Gamma = (np.sin(Psi) - np.sqrt(epsilon_c - np.cos(Psi)**2)) / (np.sin(Psi) + np.sqrt(epsilon_c - np.cos(Psi)**2))
-            
-        Gamma = Gamma * D_factor * roughness_factor  
-    
+        
+        # Factor de divergencia por superficie curva
+        D_factor = 1 / np.sqrt(1 + (2 * r1 * r2) / (re * r * np.sin(Psi)))
+        
+        # Rugosidad
+        roughness_factor = np.exp(-2 * (self.Beta * self.roughness * np.sin(Psi))**2)
+        
+        Gamma = Gamma * D_factor * roughness_factor
+        
         # Factor de interferencia
         F_i = np.sqrt(1 + np.abs(Gamma)**2 + 2 * np.abs(Gamma) * np.cos(Delta + np.angle(Gamma)))
         
+        
+        
+        # lim_psi = np.deg2rad(0.1)
+        # if Psi < lim_psi:
+        #     F_i = 1
+        
+            
+        # else:
+        #     Gamma = 0
+        #     D_factor = 0
+        #     roughness_factor = 0
+        #     Psi = 0
+        #     F_i = 1
+        #     Rd = r
+                   
+        P_t = self.tx_power * self.antenna_tx_gain  # potencia de transmisión
+        E_zero = np.sqrt(ETA_ZERO * P_t / (4 * np.pi)) / Rd
+
         E_total = np.abs(E_zero) * np.abs(F_i)
-        
-        # print(f'E_total: {E_total:.2e}\t\t|E_zero|: {np.abs(E_zero):.2e}\t\t|F_i|: {np.abs(F_i):.2e}\t\t|Gamma|: {np.abs(Gamma):.2e}\n')
-        
         P_r =   (np.abs(E_total)**2 / ETA_ZERO) * \
-                (self.lambd**2 / (4 * np.pi)) * \
-                self.antenna_rx_gain                       
+                    (self.lambd**2 / (4 * np.pi)) * \
+                    self.antenna_rx_gain       
+                
+        # if Psi > lim_psi:
+        #     P_r =   (np.abs(E_total)**2 / ETA_ZERO) * \
+        #             (self.lambd**2 / (4 * np.pi)) * \
+        #             self.antenna_rx_gain
+                    
+        #     self.last_P_r = P_r
+        # else:
+        #     P_r = P_t * (ht**2 * hr**2) / (Rd**4)
+            
+        #     if self.first_aprox_run:
+        #         self.aprox_P_r_offset = (self.last_P_r - P_r) * 0.9
+        #         self.first_aprox_run = False
+                
+        #     P_r = P_r + self.aprox_P_r_offset
+                  
+        #     E_total = np.sqrt(ETA_ZERO * P_r / ((self.lambd**2 / (4 * np.pi)) * self.antenna_rx_gain))
         
         # Pérdida por espacio libre
         L_fs = (4 * np.pi * Rd / self.lambd) ** 2
         P_r_fs = (P_t / L_fs) * self.antenna_rx_gain
         E_fs = np.sqrt(ETA_ZERO * (P_r_fs / ((self.lambd**2 / (4 * np.pi)) * self.antenna_rx_gain)))
         
+        
+        # d_1 = np.sqrt(2 * re) * (np.sqrt(ht) + np.sqrt(0))
+        # d_2 = np.sqrt(2 * re) * (np.sqrt(0) + np.sqrt(hr))
+        # D_0 = d_1 + d_2
+        # d_3 = r - D_0
+        
+        # d_ns = [d_1, d_2, d_3]
+        # def calc_chi_n(n):
+        #     d_n = d_ns[n-1]
+        #     return (2*np.pi*d_n/self.lambd) / ((2*np.pi*re)**(2/3))
+        
+        # def calc_F_s_n(chi_n):
+        #     logged = (-0.048 * chi_n**3 + 1.0875 * chi_n**2 + 4.0782 * chi_n - 0.8806)
+        #     return (10 ** (logged/20))
+        
+        # def calc_N_n(n):
+        #     chi_n = calc_chi_n(n)
+        #     logged = (-0.5 + 35*np.log10(chi_n) + 10 * np.log10(calc_F_s_n(chi_n)))
+        #     return (10 ** (logged/20)), chi_n
+        
+        # N_1, chi_1 = calc_N_n(1)
+        # N_2, _ = calc_N_n(2)
+        # chi_3 = calc_chi_n(3)
+        
+       
+        # D_f = 0.000389 * (self.freq / 1e6) * ht * hr # frequency-dependent factor
+        # D_h = self.LOS_point_to_point # asymptotic term
+        # D_06 = (D_f * D_h) / (D_f + D_h) # path length of a clearance of 0.6 FFZ
+        # D_06 = D_06 * 1000 # to meters
+        
+        # L_1 = 20 * np.log10(N_1 / np.sqrt(5.656 * np.pi * chi_1))
+        # L_2 = 20 * np.log10(N_2)
+        # L_3 = 0.0086 * chi_3**3 + 0.2063 * chi_3**2 + 11.0997 * chi_3 - 0.8934
+        
+        # L = None
+        # if r >= (d_1 + d_2):
+        #     L = L_1 + L_2 - np.abs(L_3)
+        # elif r > D_06:
+        #     L = L_1 + L_2 + np.abs(L_3)
+        # else:
+        #     L = 0
+        
+        # L = 10 ** (L/10)
+        
+        # P_r = P_r / L
+        # E_total = np.sqrt(ETA_ZERO * (P_r / ((self.lambd**2 / (4 * np.pi)) * self.antenna_rx_gain)))
+        
+        
+        
+        
         return E_total, P_r, E_fs, P_r_fs, np.abs(Gamma), np.abs(F_i)
 
+    # def calculate_calc_los(self, height_tx, height_rx):
+    #     re = self.earth_radius_factor * EARTH_RADIUS
+    #     ht = height_tx
+    #     hr = height_rx
+
+    #     def delta_r_function(r):
+    #         p = (2 / np.sqrt(3)) * np.sqrt(re * (hr + ht) + r*r/4)
+    #         Xi = np.arcsin(2 * re * r * (hr - ht) / (p*p*p))
+    #         r1 = r/2 - p * np.sin(Xi/3)
+    #         r2 = r - r1
+    #         phi1 = r1/re
+    #         phi2 = r2/re
+    #         R1 = np.sqrt(ht**2 + 4 * re * (re + ht) * (np.sin(phi1 / 2)**2))
+    #         R2 = np.sqrt(hr**2 + 4 * re * (re + hr) * (np.sin(phi2 / 2)**2))
+    #         Rd = np.sqrt((hr - ht)**2 + 4 * (re + hr) * (re + ht) * (np.sin((phi1 + phi2) / 2)**2))
+    #         Delta_R = R1 + R2 - Rd
+    #         return Delta_R
+
+        
+    #     r_initial_guess = 50e0
+    #     r_solution = fsolve(delta_r_function, r_initial_guess)
+        
+    #     self.LOS_point_to_point = r_solution[0]
+    
     def calculate_calc_los(self, height_tx, height_rx):
         re = self.earth_radius_factor * EARTH_RADIUS
         ht = height_tx
-        hr = height_rx
-
-        def delta_r_function(r):
-            p = (2 / np.sqrt(3)) * np.sqrt(re * (hr + ht) + r*r/4)
-            Xi = np.arcsin(2 * re * r * (hr - ht) / (p*p*p))
-            r1 = r/2 - p * np.sin(Xi/3)
-            r2 = r - r1
-            phi1 = r1/re
-            phi2 = r2/re
-            R1 = np.sqrt(ht**2 + 4 * re * (re + ht) * (np.sin(phi1 / 2)**2))
-            R2 = np.sqrt(hr**2 + 4 * re * (re + hr) * (np.sin(phi2 / 2)**2))
-            Rd = np.sqrt((hr - ht)**2 + 4 * (re + hr) * (re + ht) * (np.sin((phi1 + phi2) / 2)**2))
-            Delta_R = R1 + R2 - Rd
-            return Delta_R
-
-        
-        r_initial_guess = 50e0
-        r_solution = fsolve(delta_r_function, r_initial_guess)
-        
-        self.LOS_point_to_point = r_solution[0]
+        self.LOS_point_to_point = np.sqrt(2 * re) * (np.sqrt(height_tx) + np.sqrt(height_rx)) # radio horizonte
         
     
     def calculate_get_los(self):
@@ -173,6 +265,8 @@ class PropagationCalculator:
     def calculate_variation_with_distance(self, height_tx, height_rx, distance_start, distance_end, distance_step):
         distances = np.arange(distance_start, distance_end+distance_step, distance_step)
         
+        distances = distances[distances <= self.LOS_point_to_point]
+        
         E_totals = []
         P_rs = []
         E_fss = []
@@ -183,10 +277,6 @@ class PropagationCalculator:
         for distance in distances:
             E_total, P_r, E_fs, P_r_fs, Gamma, F_i = self.calculate_point_to_point(height_tx, height_rx, distance)
             
-            if E_total is None or P_r is None:
-                distances = distances[:len(E_totals)]
-                break
-            
             E_totals.append(E_total)
             P_rs.append(P_r)
             E_fss.append(E_fs)
@@ -194,9 +284,9 @@ class PropagationCalculator:
             Gammas.append(Gamma)
             F_is.append(F_i)
             
-            
             # distancia máxima dentro de radiohorizonte, stepizada
             self.max_distance = distances[len(E_totals)-1]
+            # self.max_distance = self.LOS_point_to_point
         
         return distances, E_totals, P_rs, E_fss, P_r_fss, Gammas, F_is
     
@@ -263,7 +353,7 @@ class PropagationCalculator:
             if vary_tx:
                 self.calculate_calc_los(height, height_fixed)
                 
-                if self.LOS_point_to_point is not None and distance >= self.LOS_point_to_point:
+                if distance >= self.LOS_point_to_point:
                     continue
                 
                 E_total, P_r, _, _, Gamma, F_i = self.calculate_point_to_point(height, height_fixed, distance)
@@ -272,7 +362,7 @@ class PropagationCalculator:
             else:
                 self.calculate_calc_los(height_fixed, height)
                 
-                if self.LOS_point_to_point is not None and distance >= self.LOS_point_to_point:
+                if distance >= self.LOS_point_to_point:
                     continue
                 
                 E_total, P_r, _, _, Gamma, F_i = self.calculate_point_to_point(height_fixed, height, distance)
